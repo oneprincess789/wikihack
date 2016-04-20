2cce46b6cf781c15d571a012fdc6cb673b052d8e
Elizabeth Gruber <elizabeth.gruber@gmail.com>
2016-04-19 20:57:17 -0400

    all

---
+++ js/script.js
@@ -0,0 +1,22 @@
+angular.module('myApp', ["ngSanitize"])
+
+.controller('homeController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.input;
+    $scope.beginUrl = "http://en.wikipedia.org/w/api.php?titles="
+    $scope.endUrl = "&rawcontinue=true&action=query&format=json&prop=extracts&callback=JSON_CALLBACK"
+    $scope.showResult = false;
+
+
+    $scope.searchButton = function () {
+        $http.jsonp($scope.beginUrl + $scope.input + $scope.endUrl).then(function (data) {
+            var obj = data.data.query.pages;
+            var pageId = Object.keys(obj)[0];
+            console.log(data);
+            $scope.wikiObject = obj[pageId].extract; 
+            $scope.showResult = true;
+            $scope.input = "";
+        })
+
+    }; 
+}]);
