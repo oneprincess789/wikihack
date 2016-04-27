98fe91c2ab0283dd1af1542621756944a9218c9d
Elizabeth Gruber <elizabeth.gruber@gmail.com>
2016-04-26 22:05:19 -0400

    sanitize

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ angular.module('myApp', ["ngSanitize"])
     $scope.beginUrl = "http://en.wikipedia.org/w/api.php?titles="
     $scope.endUrl = "&rawcontinue=true&action=query&format=json&prop=extracts&callback=JSON_CALLBACK"
     $scope.showResult = false;
+    $scope.loading = false;
 
 
     $scope.searchButton = function () {
+        $scope.loading = true;
         $http.jsonp($scope.beginUrl + $scope.input + $scope.endUrl).then(function (data) {
             var obj = data.data.query.pages;
             var pageId = Object.keys(obj)[0];
@@ -16,6 +18,7 @@ angular.module('myApp', ["ngSanitize"])
             $scope.wikiObject = obj[pageId].extract; 
             $scope.showResult = true;
             $scope.input = "";
+            $scope.loading = false;
         })
 
     }; 
